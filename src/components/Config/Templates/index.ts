0c2e6f4f00f3e2d6a369693ff4e3a0fee50a2a3d
peternimmo449393 <peter.nimmo@cox.net>
2026-07-18 11:24:43 -0700

    Resolve Batch B (messaging) and Batch C (Config/System, TemplateBuilder) orphan clusters

--- src/components/Config/Templates/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./TemplatesTab";
-export * from "./AdminTemplateList";
-export * from "./TemplateRenderer";
-export * from "./TemplateMetadataPanel";
-export * from "./TemplateReviewActions";
--- src/pages/Synoptic/Delegate/index.ts
+++
@@ -1,3 +0,0 @@
-export { default } from './DelegateModal';
-// If you have types in that folder, you can export them too:
-// export * from './delegateTypes';
